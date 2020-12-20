8729974b70298188a8316e06e7d94020fee4fa16
Matt Robinson <mrmrrobinson@gmail.com>
2020-12-20 14:39:30 +0000

    [Added] - Git ignore. - Formatting, needs cleanup collector though.

--- main.go
+++ main.go
@@ -13,17 +13,24 @@ import (
 // PictureInformation contains the information
 // about the location.
 type PictureInformation struct {
-	Date  string `json:"date"`
-	Link  string `json:"link"`
-	Title string `json:"title"`
+	Date  string   `json:"date"`
+	Link  []string `json:"link"`
+	Title string   `json:"title"`
 }
 
-func writeToFile(fileName string, content string) {
+func writeToFile(fileName string, content []byte) {
+	if content == nil {
+		return
+	}
+	if len(content) < 3 {
+		return
+	}
+	contentBracketFix := content[1 : len(content)-1]
 	f, err := os.OpenFile(fileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
-	if _, err := f.Write([]byte(content)); err != nil {
+	if _, err := f.Write(contentBracketFix); err != nil {
 		log.Fatal(err)
 	}
 	if err := f.Close(); err != nil {
@@ -38,20 +45,22 @@ func main() {
 
 	// Find and visit all links
 	c.OnHTML("article", func(e *colly.HTMLElement) {
-		var pictureURL string
+		var pictureURLs []string
 
 		linkSrc, _ := e.DOM.Find("div").Find("a").Find("img").Attr("srcset")
 		links := strings.Split(linkSrc, " ")
 
 		// Refers to the sizes 300, 1024, 1920. We only care about 1920.
-		if len(links) == 6 {
-			pictureURL = links[len(links)-2]
+		for _, link := range links {
+			if link[0:4] == "http" {
+				pictureURLs = append(pictureURLs, link)
+			}
 		}
 
 		// Add the data to a struct.
 		newPicture := PictureInformation{
 			Date:  e.DOM.Find(".date").Text(),
-			Link:  pictureURL,
+			Link:  pictureURLs,
 			Title: e.DOM.Find(".entry-title").Text(),
 		}
 
@@ -59,8 +68,7 @@ func main() {
 		pictures = append(pictures, newPicture)
 
 	})
-
-	for i := 1; i < 5; i++ {
+	for i := 1; i < 608; i++ {
 		if i == 1 {
 			c.Visit("https://windows10spotlight.com/")
 		} else {
@@ -70,7 +78,7 @@ func main() {
 		if err != nil {
 			fmt.Println(err)
 		}
-		writeToFile("results.json", string(jsonMarshsal))
+		writeToFile("results.json", jsonMarshsal)
 		pictures = nil
 	}
 }
